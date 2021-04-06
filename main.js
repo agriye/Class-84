f8ce7bd0ba38afc1ac354f7d3385ee78f87f7877
agriye <77063924+agriye@users.noreply.github.com>
2021-04-06 14:45:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+rover_width = 100;
+rover_height = 90;
+rover_x = 10;
+rover_y = 10;
+background_img = "mars.jpg";
+rover_img = "rover.png";
+
+function add(){
+    background_imgTag = new Image();//Defining a var with a new image
+    background_imgTag.onload = uploadBackground; //Setting a function, onloading this var
+    background_imgTag.src= background_img;//Load Img
+
+    rover_imgTag = new Image();//Defining a var with a new image
+    rover_imgTag.onload = uploadrover;
+    rover_imgTag.src = rover_img;
+
+}
+function uploadBackground(){
+    ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+}
+function uploadrover(){
+    ctx.drawImage(rover_img,rover_x,rover_y,rover_width,rover_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e){
+keyPressed = e.keyCode;
+console.log (keyPressed);
+
+if (keyPressed=='38'){
+    up();
+    console.log ("up");
+}
+
+if(keyPressed=='40'){
+    down();
+    console.log ("down");
+}
+if (keyPressed=='37'){
+    left();
+    console.log ("left");
+}
+if (keyPressed=='39'){
+    right();
+    console.log ("right");
+}
+}
